c19f3529c70e74a2e4d6934f7f050ce41967b71c
Alita <6499508+hashtag-squirrel@users.noreply.github.com>
2025-11-25 12:00:50 +0100

    Update app.py
    Added case for car already started/stopped

--- CarGame/app.py
+++ CarGame/app.py
@@ -1,28 +1,24 @@
-# input help:
-# start - start the car 
-# stop - stop the car 
-# quit - exit
-
-# other things: "I don't understand that"
-
-# start: "Car started... Ready to go"
-# stop: "Car stopped."
-# exit: quits the program
-
-exit = False
-
-while not exit:
-    command = input(">").lower()
-
+while True:
+    command = input("> ").lower()
+    is_running = False
+    
     if command == "help":
         print('''
 start - to start the car
 stop - to stop the car
 quit - exit''')
     elif command == "start":
-        print("Car started... Ready to go")
+        if is_running == True:
+            print("The car is already started.")
+        else:
+            print("Car started... Ready to go")
+            is_running = True
     elif command == "stop":
-        print("Car stopped.")
+        if is_running == False:
+            print("The car is already stopped.")
+        else:
+            print("Car stopped.")
+            is_running = False
     elif command == "quit":
         break
     else:
