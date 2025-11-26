66afeacf6997d6f9109bb4270e4ad63e5c6a691c
hashtag-squirrel <alita2000@web.de>
2025-11-26 15:23:38 +0100

    Baking Converter: initial commit

---
+++ BakingConverter/app.py
@@ -0,0 +1,7 @@
+print("Welcome to the Baking Converter!")
+
+input_amount = int(input("How much flour does the recipe require in cups? "))
+
+output_amount = input_amount * 125
+
+print(f"You require {output_amount} grams of flour for your recipe.")
