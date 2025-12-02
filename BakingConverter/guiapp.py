a28696422327eb47b376e32a7618fd610ddcc673
hashtag-squirrel <alita2000@web.de>
2025-12-02 13:23:20 +0100

    BakingConverter: GUI WIP

--- BakingConverter/guiapp.py
+++ BakingConverter/guiapp.py
@@ -1,27 +1,27 @@
 from tkinter import *
 
+# GUI constants
+LARGE_PAD = 40
+SMALL_PAD = 10
+LARGE_FONT = ("Arial", 25)
+DEFAULT_FONT = ("Arial", 15)
+
 # GUI functions
 def start():
     """Returns to start screen/New conversion"""
     clear_frame(frame1)
 
-    lbl = Label(frame1, text="Would you like to convert a single ingredient or convert a recipe file?", font=("Arial", 25))
-    lbl.pack(side="top", pady=10)
+    lbl = Label(frame1, text="Would you like to convert a single ingredient or convert a recipe file?", font=DEFAULT_FONT)
+    lbl.pack(side="top", pady=SMALL_PAD)
 
     centered_frame = Frame(frame1, width=200, height=200)
-    centered_frame.pack(side="top", pady=10)
+    centered_frame.pack(side="top", pady=SMALL_PAD)
 
-    btn = Button(centered_frame, text = "Single ingredient", command = single_ingredient)
-    btn.pack(side="left", pady=10, padx=5)
+    btn = Button(centered_frame, text = "Single ingredient", font=DEFAULT_FONT,  command = single_ingredient)
+    btn.pack(side="left", pady=SMALL_PAD, padx=SMALL_PAD)
 
-    btn = Button(centered_frame, text = "Recipe file", command = recipe_file)
-    btn.pack(side="left", pady=10, padx=5)
-
-
-def display_ingredients():
-    """Displays buttons for all available ingredients for conversion"""
-    btn = Button(frame1, text = "Single ingredient", command = single_ingredient)
-    btn.pack()
+    btn = Button(centered_frame, text = "Recipe file", font=DEFAULT_FONT, command = recipe_file)
+    btn.pack(side="left", pady=SMALL_PAD, padx=SMALL_PAD)
 
 
 def clear_frame(frame):
@@ -30,17 +30,43 @@ def clear_frame(frame):
         widget.destroy()  # deleting widget
 
 
+def submit(opt):  
+    centered_frame = Frame(frame1, width=200, height=200)
+    centered_frame.pack(side="top", pady=SMALL_PAD)
+    lbl = Label(centered_frame, text="", font=LARGE_FONT)
+    lbl.pack(side="top", pady=SMALL_PAD)
+    lbl.config(text=opt.get())
+    
+
 # Converter functions
 def single_ingredient():
     clear_frame(frame1)
-    lbl = Label(frame1, text="You chose single ingredient", font=("Arial", 25))
-    lbl.pack(side="top", pady=10)
+    lbl = Label(frame1, text="Pick your ingredient", font=LARGE_FONT)
+    lbl.pack(side="top", pady=SMALL_PAD)
+
+    centered_frame = Frame(frame1, width=200, height=200)
+    centered_frame.pack(side="top", pady=SMALL_PAD)
+    
+    # Dropdown options 
+    ingredients = []
+    for ingredient in INGREDIENTS:
+        ingredients.append(INGREDIENTS[ingredient][0])
+
+    # Selected option variable  
+    opt = StringVar(value="flour")
+
+    # Dropdown menu  
+    OptionMenu(centered_frame, opt, *ingredients).pack(side="top", pady=10)
+
+    # Button to update label  
+    btn = Button(centered_frame, text="Submit", command=submit(opt))
+    btn.pack()
 
 
 def recipe_file():
     clear_frame(frame1)
-    lbl = Label(frame1, text="You chose recipe file", font=("Arial", 25))
-    lbl.pack(side="top", pady=10)
+    lbl = Label(frame1, text="You chose recipe file", font=LARGE_FONT)
+    lbl.pack(side="top", pady=SMALL_PAD)
 
 
 def convert(ingredient, unit, amount):
@@ -61,11 +87,11 @@ root.geometry('1280x720')
 menubar = Menu(root)
 
 # File menu
-file = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="File", menu=file)
-file.add_command(label="New Conversion", command=start)
-file.add_separator()
-file.add_command(label ='Exit', command = root.destroy)
+menu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Menu", menu=menu)
+menu.add_command(label="New Conversion", command=start)
+menu.add_separator()
+menu.add_command(label ='Exit', command = root.destroy)
 
 # Help menu
 help = Menu(menubar, tearoff=0)
@@ -76,11 +102,33 @@ help.add_separator()
 help.add_command(label="About BakingConverter", command=None)
 
 # Start screen
-lbl = Label(root, text="Welcome to the Baking Converter!", font=("Arial", 25))
-lbl.pack(side="top", pady=10)
+welcome_lbl = Label(root, text="Welcome to the Baking Converter!", font=LARGE_FONT)
+welcome_lbl.pack(side="top", pady=LARGE_PAD)
 
 frame1 = Frame(root, width=300, height=300)
-frame1.pack(side="top", pady=10)
+frame1.pack(side="top", pady=SMALL_PAD)
+
+btn = Button(root, text="Home", command=start)
+btn.pack(side="bottom", pady=LARGE_PAD)
+
+
+def show():  
+    lbl.config(text=opt.get())  
+
+# Dropdown options  
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]  
+
+# Selected option variable  
+opt = StringVar(value="Monday")  
+
+# Dropdown menu  
+OptionMenu(root, opt, *days).pack()  
+
+# Button to update label  
+Button(root, text="Click Me", command=show).pack()  
+
+lbl = Label(root, text=" ")  
+lbl.pack()
 
 start()
 # display_ingredients()
